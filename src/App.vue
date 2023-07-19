6f54829e383657e6adea24e27f165eb6b9275fb4
Roger_Deng <s0905635919@gmail.com>
2023-07-19 20:29:39 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <div class="col" style="max-width: 20%;">
     <!-- 上10左20 -->
   </div>
-  <div class="col" style="max-width: 10%;">
+  <div class="col" style="max-width: 10%; min-width: 200px;">
     <div class="row m-0">
       <div class="col p-0 border-0">
         <button type="button" class="btn btn-light border-0"  @click="today">today</button>
@@ -17,34 +17,50 @@
       </div>
     </div>
   </div>
-  <div class="col" style="max-width: 20%;">
+  <div class="col" style="max-width: 7%; min-width: 150px;">
     <div class="col d-flex align-items-center p-0">
       <h3 style="margin: 0%;">{{year}}/{{month + 1}}</h3>
     </div>
   </div>
-</div>
-<div class="row" style="width: 100%; height: 90vh;">
+  <div class="col" style="max-width: 40%;">
+  </div>
   <div class="col" style="max-width: 20%;">
+    <!-- 下拉式選單: year, month, week-->
+    <select>
+      <option value="option1">Year</option>
+      <option value="option2" selected>Month</option>
+      <option value="option3">Week</option>
+    </select>
+  </div>
+</div>
+<div class="row p-0" style="width: 100%; height: 90vh;">
+  <div class="col p-0" style="max-width: 20%;">
     <!--左20%-->
   </div>
-  <div class="d-flex col" style="min-width: 80%">
+  <div class="d-flex col p-0" style="min-width: 80%">
     <div class=" d-none d-sm-block" style="width: 100%; height: 100%; ">
       <div class="row">
         <div v-for="Day in 7" :key="Day" class="col panel" style="display: block;">
           {{days[Day-1]}}
         </div>
       </div>
-      <div v-for="Week in 6" :key="Week" class="row" style=" height: 16% ">
-        <div v-for="Day in 7" :key="Day" class="col panel border" style="display: block; Height: 19vh">
+      <div v-for="Week in 6" :key="Week" class="row" style="height: 16%">
+        <div v-for="Day in 7" :key="Day" class="col panel border p-0" style="display: block Height: 19%">
+          <!-- check if user clicked down here and check if user clicked down and moved to here-->
+          <span @mousedown="handleMouseDown(index)"></span>
+          <span @mousemove="handleMouseMove(index)"></span>
+          <span @mouseup="handleMouseUp(index)"></span>
           <span v-if="checkIsToday((Week - 1) * 7 + Day)" style="background-color: rgb(35, 166, 210); color: white; border-radius: 30%;">{{caculateMonthVal((Week - 1)*7 + Day)}}</span>
           <span v-else>{{ caculateMonthVal((Week - 1) * 7 + Day) }}</span>
         </div>
       </div>
+      <!-- 按鈕-->
+      <button type="button" class="btn btn-light border-0 overlap-btn p-0" style="min-width: 80%; position: absolute; top:14%; left:19%">.</button>
+      <button type="button" class="btn btn-light border-0 overlap-btn" style="min-width: 80%; max-width: 81%;position: absolute; top:28%;">-</button>
     </div> 
   </div>
 </div>
 </template>
-
 <script>
 /*
   前端資料:
@@ -105,6 +121,8 @@
 export default {
   data() {
     return {
+      // show left top length
+      button: [[false,0,0,0],[false,0,0,0],[false,0,0,0],[false,0,0,0],[false,0,0,0][false,0,0,0]],
       days: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
       currentDate: new Date(),
       year: 0,
@@ -114,6 +132,10 @@ export default {
       lastMonthDays: 0,
       thisMonthDays: 0,
       weekFirstDay: 0,
+      mousedown : false,
+      mousedownIndex: 0,
+      mousemoveIndex: 0,
+      mouseupIndex: 0,
     };
   },
   mounted() {
@@ -201,6 +223,38 @@ export default {
         return false;
       }
     },
+    handleMouseDown(index) {
+      this.mousedown = true;
+      this.mousedownIndex = index;
+      this.mousemoveIndex = index;
+      this.mouseupIndex = index;
+    },
+    handleMouseMove(index) {
+      if (this.mousedown) {
+        this.mousemoveIndex = index;
+      }
+      // showing buttons when mouse move
+    },
+    handleMouseUp(index) {
+      if (this.mousedown) {
+        this.mousedown = false;
+        this.mouseupIndex = index;
+      }
+      // showing Popover when mouse up 
+
+    },
+    controlButtonShow() {
+      if (this.mousedownIndex > 0  && this.mousedownIndex <= 7 ) this.button[0][0] = true;
+      if (this.mousedownIndex > 7  && this.mousedownIndex <= 14) this.button[1][0] = true;
+      if (this.mousedownIndex > 14 && this.mousedownIndex <= 21) this.button[2][0] = true;
+      if (this.mousedownIndex > 21 && this.mousedownIndex <= 28) this.button[3][0] = true;
+      if (this.mousedownIndex > 28 && this.mousedownIndex <= 35) this.button[4][0] = true;
+      if (this.mousedownIndex > 35 && this.mousedownIndex <= 42) this.button[5][0] = true;
+
+    },
+    caculateButtonPosition(){
+
+    }
   }
 };
 </script>
